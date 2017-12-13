748161fddc37ba63dc50e2e83d29ce021cf457c2
Shrey97 <utkarsh.kvs1@gmail.com>
2017-12-13 18:17:35 +0530

    Improved grid layout and commented the whole thing

--- gnewsclient/GUI/GUI.py
+++ gnewsclient/GUI/GUI.py
@@ -4,9 +4,11 @@ from gnewsclient import utils
 from tkinter import ttk
 
 
+# function to set attributes(language, location, topic), and then fetch news accordingly
+# we have variables topic_query, language_query, location_query for storing attributes
 def news():
     global location_query, language_query,topic_query,edition_query,client,status
-    client.query=None
+    client.query=None # because search query overrides all other parameters
     client.topic=topic_query.get()
     if(client.topic=="Select topic"):
         client.topic = "top stories"
@@ -29,6 +31,7 @@ def news():
     return
 
 
+# to search news related to a search string
 def search():
     global search_query,client,status
     client.query= search_query.get()
@@ -36,7 +39,7 @@ def search():
     print(client.get_news())
     return
 
-
+# helper function for status bar
 def getstatus(clientX):
     x = clientX.get_config()
     y=sorted(x)
@@ -45,62 +48,68 @@ def getstatus(clientX):
         text += str(i) + ": " + str(x[i]) + "                         "
     return text
 
+# set status bar text
 def update_status(client, status):
     status['text']=getstatus(client)
     return
 
 
 
-
+#initializing window
 root = Tk()
 root.title("GNEWSCLIENT")
 
+#object making and packing
 client = gnewsclient()
 status = Label(root, text=getstatus(client), bd=1, relief=SUNKEN)
 status.pack(side=BOTTOM, fill=X)
 
 
-
+# implementing searching news
 search_frame=Frame(root)
 search_frame.pack()
 search_query = StringVar()
 query = Entry(search_frame, textvariable=search_query).grid(row=1, column=0)
 search_button = Button(search_frame,text= "search", command=search).grid(row=1, column = 1)
 
+west_frame = Frame(root)
+west_frame.pack(side=LEFT)
 
 
-tle_frame=Frame(root)
+# frame for topic, location, and edition filters
+tle_frame=Frame(west_frame)
 tle_frame.pack()
-edition_label = Label(tle_frame, text="Edition: ").grid(row=1, column=0)
+
+# implementing edition filter
+edition_label = Label(tle_frame, text="Edition: ").grid(row=1, column=0,pady=50,sticky=W)
 edition_query=StringVar()
-edition_dropdown = ttk.Combobox(tle_frame, textvariable=edition_query, values=sorted(list(utils.editionMap))).grid(row=1, column=1)
+edition_dropdown = ttk.Combobox(tle_frame, textvariable=edition_query, values=sorted(list(utils.editionMap))).grid(row=1, column=1,pady=50)
 edition_query.set("Select edition")
 
-
-topic_label = Label(tle_frame, text="Topic: ").grid(row=2, column=0)
+# implementing topic filter
+topic_label = Label(tle_frame, text="Topic: ").grid(row=2, column=0,pady=50,sticky=W)
 topic_query=StringVar()
-topic_dropdown = ttk.Combobox(tle_frame, textvariable=topic_query, values=sorted(list(utils.topicMap))).grid(row=2, column=1)
+topic_dropdown = ttk.Combobox(tle_frame, textvariable=topic_query, values=sorted(list(utils.topicMap))).grid(row=2, column=1,pady=50)
 topic_query.set("Select topic")
 
 
-
-language_label = Label(tle_frame, text="Language: ").grid(row=3, column=0)
+# implementing language filter
+language_label = Label(tle_frame, text="Language: ").grid(row=3, column=0,pady=50,sticky=W)
 language_query=StringVar()
-language_dropdown = ttk.Combobox(tle_frame, textvariable=language_query, values=sorted(list(utils.langMap))).grid(row=3, column=1)
+language_dropdown = ttk.Combobox(tle_frame, textvariable=language_query, values=sorted(list(utils.langMap))).grid(row=3, column=1,pady=50)
 language_query.set("Select language")
 
 
 
-
-location_label= Label(tle_frame,text="Location: ").grid(row=4,column=0)
+# implementing location filter
+location_label= Label(tle_frame,text="Location: ").grid(row=4,column=0,pady=50,sticky=W)
 location_query = StringVar()
-location = Entry(tle_frame, textvariable=location_query).grid(row=4, column=1)
+location = Entry(tle_frame, textvariable=location_query).grid(row=4, column=1,pady=50)
 
 
 
-
-getnews = Button(root, command=news,text = "Get News!")
-getnews.pack()
+# button to fetch news
+getnews = Button(tle_frame, command=news,text = "Get News!").grid(row=6,column=0,columnspan=2, pady=25)
 
 
 
